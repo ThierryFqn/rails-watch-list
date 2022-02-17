202a50a0a62aeca452d5ca190be0c9c6a44a6bf2
Thierry Fonquernie <thierry.fonquernie@gmail.com>
2022-02-17 18:14:11 +0100

    Watch list done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :lists do
+    resources :bookmarks, only: %w(new create)
+  end
+  resources :bookmarks, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Movie.destroy_all
+
+puts "Creating Movies...🎥"
+
+url = "http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>"
+movie_serialized = URI.open(url).read
+movie = JSON.parse(movie_serialized)
+
+movie_arr = movie['results']
+english_movies = movie_arr.select { |m| m['original_language'] == 'en' }
+
+english_movies.each do |movie|
+    Movie.create(
+    title: movie["original_title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}",
+    rating: movie["vote_average"])
+    puts "1 movie created..✅"
+end
+
+puts "Seed done, enjoy !"
